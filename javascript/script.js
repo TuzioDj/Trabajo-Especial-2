bb8fc1e8acf7d9c446230627f049b378f5368ac6
TuzioDj <90795509+TuzioDj@users.noreply.github.com>
2022-06-07 12:29:02 -0300

    Pagina completa funcionando (Falta ultimar detalles)

--- javascript/script.js
+++ javascript/script.js
@@ -16,12 +16,13 @@ function displayMenu(){
 
     let captchaCode = document.querySelector("#captchaText"),
     captchaInsert = document.querySelector("#captchaInsert"),
-    captchaBtn = document.querySelector (".formButton"),
-    captchaVerificated = document.querySelector("#captchaVerification");
-    cpuInsert = document.querySelector("#CPU")
-    motherInsert = document.querySelector("#Mother")
-    gpuInsert = document.querySelector("#GPU")
-    powersupplyInsert = document.querySelector("#PowerSupply")
+    captchaBtn = document.querySelector("#enviar"),
+    captchaVerificated = document.querySelector("#captchaVerification"),
+    cpuInsert = document.querySelector("#CPU"),
+    motherInsert = document.querySelector("#Mother"),
+    gpuInsert = document.querySelector("#GPU"),
+    powersupplyInsert = document.querySelector("#PowerSupply"),
+    captchaInserted = captchaInsert.value;
 
     let characteres =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
                         'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
@@ -33,34 +34,103 @@ function displayMenu(){
             captchaCode.textContent += randomCharacter;
         }
     }
-    captchaBtn.addEventListener("click", e=>{
-        e.preventDefault()
+    function captchaVerification(){
         captchaVerificated.style.visibility = "visible";
-        let captchaInserted = captchaInsert.value;
         if (cpuInsert.value == "") {
             captchaVerificated.style.color = "rgb(255, 0, 0)";
             captchaVerificated.textContent = "Primero llená el campo del CPU"
+            return false;
         }
         else if (motherInsert.value == "") {
             captchaVerificated.style.color = "rgb(255, 0, 0)";
             captchaVerificated.textContent = "Primero llená el campo de la Motherboard"
+            return false;
         }
         else if (gpuInsert.value == "") {
             captchaVerificated.style.color = "rgb(255, 0, 0)";
             captchaVerificated.textContent = "Primero llená el campo de la GPU"
+            return false;
         }
         else if (powersupplyInsert.value == "") {
             captchaVerificated.style.color = "rgb(255, 0, 0)";
             captchaVerificated.textContent = "Primero llená el campo de la Fuente de poder"
+            return false;
         }
-        else if(captchaInserted == captchaCode.textContent){
-            captchaVerificated.style.color = "rgb(0, 255, 0)";
-            captchaVerificated.textContent = "Felicidades!, no sos un robot :D"
-        }
-        else{
+        else if(captchaInsert.value != captchaCode.textContent){
             captchaVerificated.style.color = "rgb(255, 0, 0)";
             captchaVerificated.textContent = "Sos un robot >:("
+            return false;
         }
-    })
+        else if(captchaInsert.value == captchaCode.textContent){
+            captchaVerificated.style.color = "rgb(0, 255, 0)";
+            captchaVerificated.textContent = "Felicidades!, no sos un robot :D"
+            return true;
+        }
+    }
 
-    newCaptcha()
+// TABLA Y DATOS
+    
+    table = document.querySelector(".formTable"),
+    enviar3 = document.querySelector("#enviar3"),
+    borrarUno = document.querySelector("#borrarUno");
+    borrarTodo = document.querySelector("#borrarTodo");
+
+    captchaBtn.addEventListener('click',createItem);
+    enviar3.addEventListener('click', create3Items);
+    borrarUno.addEventListener('click',cleanItem);
+    borrarTodo.addEventListener('click',cleanAllItems);
+
+    let PC = [];
+    function sendInfoAndCreateTable(){
+        let newPC ={
+            "CPU": cpuInsert.value,
+            "Mother": motherInsert.value,
+            "GPU": gpuInsert.value,
+            "Fuente": powersupplyInsert.value
+        }
+        PC.push(newPC);
+        table.innerHTML +=  "<tr id=" + "item" + PC.length + 
+                                "><td>" + newPC.CPU + "</td>" + 
+                                "<td>" + newPC.Mother + "</td>" + 
+                                "<td>" + newPC.GPU + "</td>" + 
+                                "<td>" + newPC.Fuente + "</td>" +
+                            "</tr>";
+        console.log(PC);
+    }
+    function createItem(){
+        captchaState = captchaVerification();
+        console.log(captchaState);
+        if(captchaState == true){
+            sendInfoAndCreateTable();
+            cleanInputs();
+        }
+    }
+    function create3Items(){
+        captchaState = captchaVerification();
+        if(captchaState == true){
+            for (let i = 0; i < 3; i++) {
+                sendInfoAndCreateTable()  
+            }
+        }
+        cleanInputs();
+    }
+    function cleanAllItems(){
+        while(PC.length > 0){
+            cleanItem();
+        }
+    }
+    function cleanItem(){
+        let lastItem = document.querySelector("#item" + PC.length)
+        if(PC.length > 0){
+            lastItem.remove();
+            PC.splice(PC.length-1)
+            console.log("Item eliminado: " + PC);
+        }
+    }
+    function cleanInputs(){
+        motherInsert.value="";
+        cpuInsert.value="";
+        gpuInsert.value="";
+        powersupplyInsert.value="";
+    }
+    newCaptcha();
